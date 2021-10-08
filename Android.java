96251a6a460f462748cecf50424ce68c6b6f722d
MeetPatel11 <72241988+MeetPatel11@users.noreply.github.com>
2021-10-08 21:38:32 +0530

    Create Android.java

---
+++ Android.java
@@ -0,0 +1,20 @@
+package com.phone;
+
+public class Android implements OS {
+
+    public void security() {
+        System.out.println("My security is good but some apps can broke it ");
+    }
+
+    public void cost() {
+        System.out.println("I am free OS ");
+    }
+
+    public void availableApps() {
+        System.out.println("I have too much apps and you can create one easily ");
+    }
+
+    public void future() {
+        System.out.println("My future is very good ");
+    }
+}
